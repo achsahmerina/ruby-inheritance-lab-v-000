6a040de527df870ee04b3a455e9b582f665abd1e
Achsah Abraham <achsahmp@gmail.com>
2018-10-01 17:50:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,13 @@
-class Student
-  attr_accessor :first_name, :last_name
+class Student < User
+  attr_accessor :knowledge, :learn
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(knowledge, learn)
+    @knowledge = []
   end
 
-  def first_name
-    @first_name
-  end
-  def last_name
-    @last_name
-  end
+def learn(sentence)
+  knowledge << sentence
+end
 
 
 end
